277d4ebc496ced87557969e09dc209109de33a96
ViktoriaDzen <109275880+ViktoriaDzen@users.noreply.github.com>
2022-07-21 22:24:09 +0300

    added current date

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = (document.querySelector("#temperature").innerHTML =
@@ -12,6 +27,8 @@ function displayTemperature(response) {
   let windSpeed = (document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   ));
+  let date = document.querySelector("#weather-date");
+  date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "c3bcb3bc618a5bbe6638510330760ed9";
